2cafa90ed4b291b5724e9b3b24924edaafb21ae6
Jaco <thiartjaco518@gmail.com>
2024-08-06 20:08:45 +0200

    set

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -64,7 +64,7 @@ const PropertyPage = () => {
           <section className='bg-blue-50'>
             <div className='container m-auto py-10 px-6'>
               <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
-              <PropertyDetails property={property} />
+                <PropertyDetails property={property} />
                 <aside className='space-y-4'>
                   <BookmarkButton/>
                   <ShareButtons property={property}/>
