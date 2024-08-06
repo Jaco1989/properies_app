3fc135d21ec8e06a8b16b7d3d1dede2907c89d73
Jaco <thiartjaco518@gmail.com>
2024-08-06 14:41:07 +0200

    set fetch api function

--- utils/requests.js
+++ utils/requests.js
@@ -1,7 +1,7 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all properties
-export async function fetchProperties() {
+async function fetchProperties() {
   try {
     // Handle the case where the domain is not available yet
     if (!apiDomain) {
@@ -22,3 +22,26 @@ export async function fetchProperties() {
     return [];
   }
 }
+
+// Fetch single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
