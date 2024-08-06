0372f0b532e203cf9d54efdcfe9d0bb19e62ae99
Jaco <thiartjaco518@gmail.com>
2024-08-06 15:12:19 +0200

    set single property page useEffect to fetch from the client

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,44 @@
+"use client"
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
+import Spinner from '@/components/Spinner';
+import { FaArrowLeft } from 'react-icons/fa'
+
 const PropertyPage = () => {
+
+  const { id } = useParams();
+
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error('Error fetching property:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+
+  if (!property && !loading) {
+    return (
+      <h1 className='text-center text-2xl font-bold mt-10'>
+        Property Not Found
+      </h1>
+    );
+  }
+
     return (
       <div>
         <h1>PropertyPage Home</h1>
