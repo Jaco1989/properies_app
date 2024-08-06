c64ed4d691d3058df6c6f6540c82b122a3e4f364
Jaco <thiartjaco518@gmail.com>
2024-08-06 19:52:36 +0200

    set property details

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -9,6 +9,7 @@ import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import BookmarkButton from '@/components/BookmarkButton';
 import ShareButtons from '@/components/ShareButtons';
 import PropertyContactForm from '@/components/PropertyContactForm';
+import PropertyDetails from '@/components/PropertyDetails';
 
 const PropertyPage = () => {
 
@@ -63,6 +64,7 @@ const PropertyPage = () => {
           <section className='bg-blue-50'>
             <div className='container m-auto py-10 px-6'>
               <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+              <PropertyDetails property={property} />
                 <aside className='space-y-4'>
                   <BookmarkButton/>
                   <ShareButtons property={property}/>
